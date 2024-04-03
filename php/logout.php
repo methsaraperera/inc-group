e57e0af3c4588aa980ae821a7c2eb0da74b1d04f
Methsara Perera <71716579+methsaraperera@users.noreply.github.com>
2024-04-03 03:17:38 -0400

    Delete php directory

--- php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['uid']);
-    unset($_SESSION['insid']);
-    unset($_SESSION['instructor']);
-    echo '<meta http-equiv="refresh" content="0; URL=../index.php"/>';
-?>
