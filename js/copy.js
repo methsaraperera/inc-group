b85c8e6a8cd2c56c0d309cb3df79ab1c3497124f
Methsara Perera <71716579+methsaraperera@users.noreply.github.com>
2024-04-03 03:18:40 -0400

    Delete js directory

--- js/copy.js
+++
@@ -1,9 +0,0 @@
-function copyText(elementId) {
-    var textToCopy = document.getElementById(elementId).innerText;
-    var tempInput = document.createElement("input");
-    tempInput.value = textToCopy;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-}
